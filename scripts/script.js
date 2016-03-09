705fb31634ff71b588cb1a77a684d4ed4755957a
Abin Thaha Azeez <abin@qburst.com>
2016-03-09 15:34:24 +0530

    Passing data down to childres completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,54 @@
 var CommentBox = React.createClass({
-    render: function() {
-        return (
-            <div className="commentBox">
-                <h1>Comments</h1>
-                <Comments/>
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
 });
 var divStyle = {
     color: 'red',
 }
-var Comments = React.createClass({
-    render: function() {
-        return (
-            <h2 className="commentAuthor" style={divStyle}>Commmmmmmmmmments</h2>
-        );
-    }
+var array = [
+    {name: 'Abin Thaha', id: '01'},
+    {name: 'Lavya', id: '02'}
+]
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+          <Comment author="Pete Hunt">This is one comment</Comment>
+          <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
 });
 ReactDOM.render(
-  React.createElement(CommentBox, null),
+  <CommentBox data={array} />,
   document.getElementById('content')
 );
