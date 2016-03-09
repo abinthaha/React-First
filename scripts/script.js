cd9a2ce72f7d4976df2defd7efdd93018f3b90c7
Abin Thaha Azeez <abin@qburst.com>
2016-03-09 12:50:39 +0530

    Multiple components in heirarchichal order

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,22 @@
-var CommentBox = React.createClass({displayName: 'CommentBox',
-  render: function() {
-    return (
-      React.createElement('div', {className: "commentBox"},
-        "Hello, world! I am a CommentBox."
-      )
-    );
-  }
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <Comments/>
+            </div>
+        );
+    }
+});
+var divStyle = {
+    color: 'red',
+}
+var Comments = React.createClass({
+    render: function() {
+        return (
+            <h2 className="commentAuthor" style={divStyle}>Commmmmmmmmmments</h2>
+        );
+    }
 });
 ReactDOM.render(
   React.createElement(CommentBox, null),
